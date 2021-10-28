2dce84735ba36ae57ba0c5de99151adade172122
wbadmin <>
2021-10-28 19:47:29 +0000

    {/src/main/java/com/facebank/clients/PersonalData.java}

--- src/main/java/com/facebank/clients/PersonalData.java
+++ src/main/java/com/facebank/clients/PersonalData.java
@@ -10,7 +10,7 @@ public class PersonalData implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	@javax.persistence.GeneratedValue(generator = "PERSONALDATA_ID_GENERATOR", strategy = javax.persistence.GenerationType.SEQUENCE)
+	@javax.persistence.GeneratedValue(generator = "PERSONALDATA_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
 	@javax.persistence.Id
 	@javax.persistence.SequenceGenerator(sequenceName = "PERSONALDATA_ID_SEQ", name = "PERSONALDATA_ID_GENERATOR")
 	private java.lang.Long id;
